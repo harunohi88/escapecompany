248a5d133ee5c9ff71c856d86f97b4edb9167bd8
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-03-31 13:16:02 +0900

    스턴 버튼 따로 추가 및 모바일 120 빌드 추가

--- Assets/02.Scripts/Player_Main/Manager/BGMManager.cs
+++ Assets/02.Scripts/Player_Main/Manager/BGMManager.cs
@@ -9,7 +9,6 @@ public class BGMManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // 싱글톤 유지
         } else if (Instance != this)
         {
             Destroy(gameObject); // 기존 인스턴스가 있다면 삭제
--- Assets/02.Scripts/Player_Main/Manager/GameManager.cs
+++ Assets/02.Scripts/Player_Main/Manager/GameManager.cs
@@ -13,12 +13,18 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject); // 싱글톤 유지
-        }
-        else if (Instance != this)
+        } else if (Instance != this)
         {
             Destroy(gameObject); // 기존 인스턴스가 있다면 삭제
         }
     }
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0; // VSync 비활성화
+        Application.targetFrameRate = 120; // 프레임을 120으로 고정
+    }
+
 
     // 씬 전환 함수
     public void LoadScene()
